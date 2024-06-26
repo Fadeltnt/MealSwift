28e259eb27b4b73911d54632fc67c293e91f8e6e
Fadeltnt <101524603+Fadeltnt@users.noreply.github.com>
2024-06-26 16:06:52 -0400

    Adding Navbar and header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+const Header= () => {
+    return (
+        <div className="header">
+            <div className="header-contents">
+                <h2>Order your favourite food here</h2>
+                <p>Discover the ultimate meal delivery experience with ExpressEats! Whether you’re craving a gourmet feast, a quick bite, or a healthy meal, we’ve got you covered. Our app brings delicious, freshly prepared food from your favorite local restaurants straight to your doorstep.</p>
+                <button>View Menu</button>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
